20716d6b1cf843ad77584dce27bac2a5a5f66341
VIO CESAR MELANO YONANTA <viocesar24@gmail.com>
2025-02-19 10:23:23 +0700

    Update footer.php

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -65,7 +65,7 @@
 </div>
 <footer class="footer mt-auto">
     <div class="container-fluid text-center text-dark bg-secondary bg-gradient bg-opacity-25">
-        <em>Made with Codeigniter 4 & Bootstrap v5.2.0 &copy; 2022 by Inspektorat Kabupaten Kediri</em>
+        <em>Made with Codeigniter 4 & Bootstrap v5.2.0 &copy; 2022 - <?php echo date('Y'); ?> by Inspektorat Kabupaten Kediri</em>
     </div>
 </footer>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js"></script>
